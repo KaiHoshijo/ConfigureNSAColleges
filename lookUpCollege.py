1420818aa3460ae6bae732e53a5682c1929df539
KaiHoshijo <34174416+KaiHoshijo@users.noreply.github.com>
2020-05-22 20:41:40 -0600

    Add files via upload

--- lookUpCollege.py
+++ lookUpCollege.py
@@ -8,13 +8,14 @@ colleges = NSAcolleges.writeToCollege()
 collegeInformation = {}
 
 for state in colleges: 
+# state = "Alabama"
     for college in colleges[state]:
         session = requests.Session()
 
         collegeName = college.lower().replace(" ", "-")
         # print(state + " " + college)
         response = session.get("https://www.collegesimply.com/colleges/" + state.lower() + "/" + collegeName)
-
+        # print(response)
         soup = BeautifulSoup(response.text, 'html.parser')
         information = soup.find(class_ = "col-12 col-md-8")
 
@@ -22,6 +23,16 @@ for state in colleges:
             # print("failure") 
             continue
         else:
+            initial = {
+                "National Rank" : "unavailable, ", 
+                "Applications Accepted" : "unavailable, ", 
+                "In state tuition" : "unavailable, ", 
+                "Out of State Tuition" : "unavailable, ", 
+                "On Campus Housing" : "unavailable, ", 
+                "SAT" : "unavailable, ", 
+                "GPA" : "unavailable, ", 
+                "Applicant Competition" : "unavailable, "
+            }
             parentInformation = information.find_parent()
             trList = parentInformation.findAll('tr') # further dive into all the information
             colList = parentInformation.findAll(class_ = "col-12 col-md-6") + parentInformation.findAll(class_ = "col-12 col-md-8")
@@ -32,9 +43,18 @@ for state in colleges:
                     valueList = i.findAll(class_ = 'display-3')
                     if valueList == None: value = i.findAll(class_ = 'display-3 mb-0')
                     for name, value in zip(nameList[1:], valueList[1:]):
-                        if (name.get_text().strip() not in ['Admission Standards', 'Bachelor\'s Degrees Graduation Rate', 'Average Salary After 10 Years']):
+                        name = name.get_text().strip()
+                        if (name in ["National Rank", "Applications Accepted"]):
                             # print(name.get_text().strip() + " : " + value.get_text().strip())
-                            totalInfo.append(name.get_text().strip() + " : " + value.get_text().strip())
+                            # totalInfo.append(value.get_text().strip() + ", ")
+                            initial[name] = value.get_text().strip() + ", "
+                        elif ('tuition' in name.lower() or "price" in name.lower()):
+                            print(name + " : " + "".join(value.get_text().strip().split(",")) + ', ')
+                            if (state.lower() in name.lower() or "In State" in name):
+                                
+                                initial["In state tuition"] = "".join(value.get_text().strip().split(",")) + ", "
+                            else:
+                                initial["Out of State Tuition"] = "".join(value.get_text().strip().split(",")) + ", "
             for tr in trList:
                 tdList = tr.findAll('td')
                 inState = False
@@ -43,11 +63,11 @@ for state in colleges:
                     if name in ['SAT', 'GPA', 'On Campus Housing', 'Applicant Competition']:
                         value = tdList[tdIndex+1].get_text()
                         # print(name + ' : ' + value)
-                        totalInfo.append(name + " : " + value)
-            collegeInformation[college] = totalInfo
-
-            # for info in collegeInformation:
-            #     print(info + " : " + collegeInformation[info])
+                        initial[name] = value + ", "
+            collegeInformation[college] = [initial[requirement] for requirement in initial]
+            # print(initial)
+                # for info in collegeInformation:
+                #     print(info + " : " + collegeInformation[info])
 
 # print(collegeInformation.keys())
 
@@ -61,6 +81,7 @@ for state in colleges:
             totalInfomation[college] = information
     newCollegeList[state] = totalInfomation
 
+# print("".join(newCollegeList["Alabama"]["Auburn University"]))
 writeToFile.writeToCollege(newCollegeList)
 # print(newCollegeList.keys())
 # print(newCollegeList["Colorado"].keys())
--- writeToFile.py
+++ writeToFile.py
@@ -2,11 +2,10 @@ from csv import writer
 
 def writeToCollege(colleges):
     with open("colleges.csv", "w") as csv_file:
-        csv_writer = writer(csv_file)
-
+        # csv_writer = writer(csv_file)
+        csv_file.write("State, College, National Rank, Applications Accepted, In state tuition, Out of State Tuition, On Campus Housing, SAT, GPA, Applicant Competition, \n")
         for state in colleges:
-            csv_writer.writerow([state])
+            # csv_writer.writerow([state])
             for college in colleges[state]:
-                csv_writer.writerow(["".join(["\t", college])])
-                for requirements in colleges[state][college]:
-                    csv_writer.writerow(["".join(["\t\t", requirements])])
+                # csv_writer.writerow(["".join(["\t", college])])
+                csv_file.write("".join([state+", ", college + ", ", "".join(colleges[state][college]), "\n"]))
